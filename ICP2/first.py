e5048b7474434ad4beb7705763733f55340c7ca7
dsantosh1111 <31327130+dsantosh1111@users.noreply.github.com>
2019-02-02 15:49:09 -0600

    icp2

--- ICP2/first.py
+++
@@ -1,6 +0,0 @@
-n = eval(input("How many  numbers  do you have? "))
-sum  = 0.0
-for i in range(n):
-    x = int(input("Enter the numbers  >> "))
-    sum  = sum  + x.split('', n)
-print("\nThe average  of the numbers  is", sum  / n)
